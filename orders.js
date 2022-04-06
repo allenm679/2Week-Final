13d23341cf318bd1421aa1e3abf9cdaf32cff8ec
Allen Mounivong <102091039+allenm679@users.noreply.github.com>
2022-04-06 17:05:27 -0500

    Update orders.js

--- orders.js
+++ orders.js
@@ -31,6 +31,21 @@
 
 //CODE HERE
 
+//Redo below
+
+class Ticket {
+    constructor(items, orderTime, customerId) {
+        this.items = items
+        this.orderTime = orderTime
+        this.customerId = customerId
+        this.status = 'queued'
+    }
+    updateStatus(newStatus){
+        this.status = newStatus
+        console.log(`The order for customer ${this.customerId} is now ${this.status}`)
+    }
+}
+
 
 
 /*
@@ -46,6 +61,10 @@
 
 //CODE HERE
 
+// Redo below
+const firstTicket = new Ticket(['pizza' , 'bread', 'soda' , '7:03 pm' , 575])
+
+console.log(firstTicket)
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +73,6 @@
 */
 
 //CODE HERE
+
+
+firstTicket.updateStatus('cooking')
